3e676856902bac511e2d66b378ca821de94995a4
Wobblyyyy <30844830+Wobblyyyy@users.noreply.github.com>
2020-05-04 18:53:41 -0400

    Optimized code so it's more efficient & lightweight.

--- src/me/wobblyyyy/experience/handlers/BlockBreakHandler.java
+++ src/me/wobblyyyy/experience/handlers/BlockBreakHandler.java
@@ -1,8 +1,10 @@
 package me.wobblyyyy.experience.handlers;
 
 import me.wobblyyyy.experience.Experience;
+import me.wobblyyyy.experience.ext.ObjectLists;
 import net.minecraft.server.v1_15_R1.NBTTagCompound;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -15,87 +17,79 @@ import java.util.concurrent.ThreadLocalRandom;
 public class BlockBreakHandler
 {
     Experience plugin;
+    ObjectLists list;
 
     public BlockBreakHandler (Experience plugin)
     {
         this.plugin = plugin;
+        list = plugin.getObjectLists();
+    }
+
+    public boolean isTool (Material item)
+    {
+        String toFind = item.toString();
+        return list.pickaxes.contains(toFind) || list.axes.contains(toFind) || list.shovels.contains(toFind);
+    }
+
+    public int getEnchant (ItemStack item, Enchantment enchant)
+    {
+        return Objects.requireNonNull(item.getItemMeta()).hasEnchant(enchant) ? item.getEnchantmentLevel(enchant) : 0;
+    }
+
+    public ItemStack getDrops (BlockBreakEvent event)
+    {
+        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+        ItemStack drop = null;
+        Block block = event.getBlock();
+        if (isTool(item.getType()))
+        {
+            int fortune = getEnchant(item, Enchantment.LOOT_BONUS_BLOCKS);
+            int smelting = getEnchant(item, plugin.getAutoSmelt());
+            if (smelting != 0 && list.smeltedDrops.containsKey(block.getType()))
+            {
+                drop = new ItemStack(list.smeltedDrops.get(block.getType()));
+            }
+            else
+            {
+                drop = list.drops.containsKey(block.getType()) ? new ItemStack(list.drops.get(block.getType())) : new ItemStack(block.getType());
+            }
+            if (fortune != 0 && list.fortuneMultiplier.containsKey(block.getType()))
+            {
+                drop.setAmount((int) Math.ceil(ThreadLocalRandom.current().nextInt(1, fortune + 1) * list.fortuneMultiplier.get(block.getType())));
+            }
+
+            return drop;
+        }
+        for (ItemStack i : event.getBlock().getDrops())
+        {
+            return i;
+        }
+        return null;
+    }
+
+    public ItemMeta updateTool (ItemStack item)
+    {
+        net.minecraft.server.v1_15_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = nmsItem.getOrCreateTag();
+        tag.setLong("counter", tag.getLong("counter") + 1);
+        item = CraftItemStack.asBukkitCopy(nmsItem);
+
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        String name = meta.hasDisplayName() ? meta.getDisplayName() : plugin.getObjectLists().defaultToolNames.get(item.getType().name());
+        meta.setDisplayName(plugin.getEngine().executeScriptFunction("newName", plugin.getConfiguration().toolCounterFormat, plugin.getConfiguration().counterFormat, name, tag.getLong("counter") - 1));
+        return meta;
     }
 
     public void handleBlockBreak (BlockBreakEvent event)
     {
-        Material blockType = event.getBlock().getType();
-        if (plugin.getObjectLists().drops.containsKey(blockType))
-        {
-            ItemStack item = null;
-            boolean notSmeltable = true;
-            if (plugin.getObjectLists().smeltedDrops.containsKey(blockType))
-            {
-                if (Objects.requireNonNull(event.getPlayer().getInventory().getItemInMainHand().getItemMeta()).hasEnchant(plugin.getAutoSmelt()))
-                {
-                    Material mat = plugin.getObjectLists().smeltedDrops.get(blockType);
-                    item = new ItemStack(mat);
-                    notSmeltable = false;
-                }
-            }
-            if (plugin.getObjectLists().drops.containsKey(blockType) && notSmeltable)
-            {
-                Material mat = plugin.getObjectLists().drops.get(blockType);
-                item = new ItemStack(mat);
-            }
-            if (plugin.getObjectLists().fortunes.containsKey(blockType) || plugin.getObjectLists().blockFortunes.containsKey(blockType))
-            {
-                int fortuneLevel = Objects.requireNonNull(event.getPlayer().getInventory().getItemInMainHand().getItemMeta()).hasEnchant(Enchantment.LOOT_BONUS_BLOCKS) ? event.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS) + 1 : 1;
-                double fortuneMultiplier = plugin.getObjectLists().fortuneMultiplier.containsKey(blockType) ? plugin.getObjectLists().fortuneMultiplier.get(blockType) : 1;
-                int random = ThreadLocalRandom.current().nextInt(1, fortuneLevel + 1);
-                int toGive = fortuneLevel != 0 ? (int) Math.ceil(random * fortuneMultiplier) : 1;
-                assert item != null;
-                item.setAmount(toGive);
-            }
-            event.getPlayer().getInventory().addItem(item);
-        }
-        else
-        {
-            for (ItemStack i : event.getBlock().getDrops())
-            {
-                event.getPlayer().getInventory().addItem(i);
-            }
-        }
-        event.setDropItems(false);
-
         ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
-        String preItem = item.getType().toString();
-        boolean isTool = false;
-        if (plugin.getObjectLists().pickaxes.contains(preItem))
+        if (isTool(item.getType()))
         {
-            plugin.getBlocksInterface().executeScriptFunction("incrementJSON", "[pickaxes][" + event.getPlayer().getName() + "]", 1);
-            isTool = true;
-        }
-        else if (plugin.getObjectLists().axes.contains(preItem))
-        {
-            plugin.getBlocksInterface().executeScriptFunction("incrementJSON", "[axes][" + event.getPlayer().getName() + "]", 1);
-            isTool = true;
-        }
-        else if (plugin.getObjectLists().shovels.contains(preItem))
-        {
-            plugin.getBlocksInterface().executeScriptFunction("incrementJSON", "[shovels][" + event.getPlayer().getName() + "]", 1);
-            isTool = true;
-        }
-
-        if (isTool)
-        {
-            net.minecraft.server.v1_15_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-            NBTTagCompound tag = nmsItem.getOrCreateTag();
-            long counter = tag.getLong("counter");
-            tag.setLong("counter", counter + 1);
-            item = CraftItemStack.asBukkitCopy(nmsItem);
-
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            String name = meta.hasDisplayName() ? meta.getDisplayName() : plugin.getObjectLists().defaultToolNames.get(item.getType().name());
-            meta.setDisplayName(plugin.getEngine().executeScriptFunction("newName", plugin.getConfiguration().toolCounterFormat, plugin.getConfiguration().counterFormat, name, counter));
-            item.setItemMeta(meta);
-
-            event.getPlayer().getInventory().setItemInMainHand(item);
+            item.setItemMeta(updateTool(item));
+            ItemStack drops = getDrops(event);
+            event.getPlayer().getInventory().addItem(drops);
+            event.setDropItems(false);
         }
     }
 }
