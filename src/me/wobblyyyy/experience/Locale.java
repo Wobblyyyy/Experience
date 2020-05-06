e482977e62fef42b47b2a82251fb512b070471bf
Wobblyyyy <30844830+Wobblyyyy@users.noreply.github.com>
2020-05-05 20:34:52 -0400

    Added Locale class.

---
+++ src/me/wobblyyyy/experience/Locale.java
@@ -0,0 +1,24 @@
+package me.wobblyyyy.experience;
+
+public class Locale
+{
+    public String prefix = "&cExperience &f&l> &7";
+
+    public String exception = "Error with Experience plugin: &c%error%";
+    public String couldNotLoad = "Could not load Experience plugin.";
+    public String reload = "Reloading all plugin files...";
+    public String reloaded = "&aFinished reloading!";
+
+    public String wroteJSON = "Wrote data to file &c%file%";
+    public String readJSON = "Read data from file &c%file%";
+
+    public String unknownCommand = "Unknown command: &c%command%";
+    public String unknownSubcommand = "Unknown subcommand: &c%subcommand%";
+    public String unknownEnchantment = "Unknown enchantment: &c%enchantment%";
+    public String unknownPlayer = "Unknown player: &c%player%";
+
+    public String enchantSuccess = "Enchanted item with &c%enchant%&7.";
+    public String enchantFailure = "Failed to enchant item with &c%enchant% &7for reason: &c%reason%";
+    public String deenchantSuccess = "Deenchanted item with &c%enchant%";
+    public String deenchantFailure = "Failed to deenchant item with &c%enchant% &7for reason: &c%reason%";
+}
